dc6c1168ceb912d8cfcf34c2b3f6d05b503d5417
PeterDevelops <PeterDinhBC@gmail.com>
2024-05-20 19:29:03 -0700

    feat: add custom photo and relevant information on page

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-
+import App from "App";
 import "../styles/PhotoListItem.scss";
 
+const PhotoListItem = (props) => {
+  return (
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+    <>
+  <img src={props.sampleDataForPhotoListItem.imageSource} alt='Photo'/>
+  <img src={props.sampleDataForPhotoListItem.profile} alt='Photo'/>
+  <div>{props.sampleDataForPhotoListItem.username}</div>
+  <div>{props.sampleDataForPhotoListItem.location["city"]} {props.sampleDataForPhotoListItem.location["country"]}</div>
+  
+  {console.log('New data')}
+  </>
+  )
 };
 
 export default PhotoListItem;
