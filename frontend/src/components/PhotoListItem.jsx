50bc17b9fb5ffe420215212cad10685f3170d473
PeterDevelops <PeterDinhBC@gmail.com>
2024-05-22 12:55:18 -0700

    fix: remove div from first picture and encapsulate bottom content

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,13 +4,13 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({sampleDataForPhotoListItem}) => {
   return (
     <>
+    <img src={sampleDataForPhotoListItem.imageSource} alt='Photo'/>
+    
       <div>
-        <img src={sampleDataForPhotoListItem.imageSource} alt='Photo'/>
-      </div>
-      
-    <img src={sampleDataForPhotoListItem.profile} alt='Photo'/>
-    <div>{sampleDataForPhotoListItem.username}</div>
-    <div>{sampleDataForPhotoListItem.location["city"]} {sampleDataForPhotoListItem.location["country"]}</div>
+        <img src={sampleDataForPhotoListItem.profile} alt='Photo'/>
+        <div>{sampleDataForPhotoListItem.username}</div>
+        <div>{sampleDataForPhotoListItem.location["city"]} {sampleDataForPhotoListItem.location["country"]}</div>
+    </div>
   </>
   )
 };
