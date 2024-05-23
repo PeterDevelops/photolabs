50376e6104ad9df0b8ab82b7cb2f0f78900b4a09
PeterDevelops <PeterDinhBC@gmail.com>
2024-05-22 17:28:08 -0700

    refactor: change string 'inactive' and 'active'

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,19 +4,18 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
-  const [heart, setHeart] = useState('inactive');
+  const [heart, setHeart] = useState('false');
 
   const likedHeart = () => {
-    setHeart(prevHeart => prevHeart === 'inactive' ? 'active' : 'inactive');
-    console.log('Heart liked');
+    setHeart(prevHeart => prevHeart === 'false' ? 'true' : 'false');
   };
 
   return (
     <div className="photo-list__fav-icon" onClick={likedHeart}>
 
       <div className="photo-list__fav-icon-svg">
-        {heart === 'inactive' && <FavIcon selected={''}/>}
-        {heart === 'active' && <FavIcon selected={'true'}/>}
+        {heart === 'false' && <FavIcon selected={''}/>}
+        {heart === 'true' && <FavIcon selected={'true'}/>}
       </div>
 
     </div>
