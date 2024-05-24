5c34bc5ec25256f1b50fbf71aab793e269548c78
PeterDevelops <PeterDinhBC@gmail.com>
2024-05-24 15:44:19 -0700

    add: print out photo details

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import photos from '../mocks/photos';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
@@ -7,12 +7,20 @@ const PhotoDetailsModal = ({isModalOpen, closeModal}) => {
   if (!isModalOpen) {
     return null;
   }
-  
+
+  const modalMap = photos.map(data => {
+    return data;
+  })
+
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={closeModal}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      
+      <div>
+      {console.log(modalMap)}
+      </div>
     </div>
   )
 };
