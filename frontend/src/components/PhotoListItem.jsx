0c97ab3ee21c92225be5719f428a608e91683d0d
PeterDevelops <PeterDinhBC@gmail.com>
2024-05-25 17:09:28 -0700

    style: make heart symbol bigger

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,28 +6,30 @@ const PhotoListItem = ({ photos, addAndDeleteFavourite, openModal }) => {
   if (!photos && !photos.urls && !photos.user) {
     return null;
   }
-  
+
   return (
     <>
-    <div className="photo-list__item">
-      
-      <div>
-    <PhotoFavButton addAndDeleteFavourite={addAndDeleteFavourite} photoID={photos.id} />
-    <img src={photos.urls.regular} alt='Photo item' className="photo-list__image" onClick={() => openModal(photos)} />
+      <div className="photo-list__item">
 
-    </div>
-      <div className="photo-list__user-details">
-        <img src={photos.user.profile} alt='User image' className="photo-list__user-profile"/>
+        <div>
+        <div>
+          <PhotoFavButton addAndDeleteFavourite={addAndDeleteFavourite} photoID={photos.id} />
+          </div>
+          <img src={photos.urls.regular} alt='Photo item' className="photo-list__image" onClick={() => openModal(photos)} />
 
-        <div className="photo-list__user-info">
-        <div>{photos.user.username}</div>
-        <div className="photo-list__user-location">{photos.location.city}, {photos.location.country}</div>
         </div>
+        <div className="photo-list__user-details">
+          <img src={photos.user.profile} alt='User image' className="photo-list__user-profile" />
 
-    </div>
-    </div>
-  </>
-  )
+          <div className="photo-list__user-info">
+            <div>{photos.user.username}</div>
+            <div className="photo-list__user-location">{photos.location.city}, {photos.location.country}</div>
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
 };
- 
+
 export default PhotoListItem;
