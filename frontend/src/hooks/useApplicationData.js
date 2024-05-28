6990af61ef8efc77debece81fe1e372023d41c90
PeterDevelops <PeterDinhBC@gmail.com>
2024-05-27 18:10:50 -0700

    remove: remove commented out code

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -53,43 +53,6 @@ function reducer(state, action) {
 
 
 const useApplicationData = function() {
-  // // state will contain the entire state of the application
-  // const [state, setState] = useState({
-  //   favourite: [],
-  //   selectedPhoto: null,
-  //   isModalOpen: false
-  // });  
-
-  // console.log('State:', state);
-
-  // // Add or remove a photo from the favourites list
-  // const updateToFavPhotoIds = (photoID) => {
-  //   setState((prevState) => {
-  //     const isFavourite = prevState.favourite.includes(photoID);
-  //     const favourite = isFavourite
-  //       ? prevState.favourite.filter((id) => id !== photoID)
-  //       : [...prevState.favourite, photoID];
-  //     return { ...prevState, favourite };  
-  //   });  
-  // };  
-
-  // // open modal
-  // const onPhotoSelect = (photo) => {
-  //   setState((prevState) => ({
-  //     ...prevState,
-  //     selectedPhoto: photo,
-  //     isModalOpen: true
-  //   }));  
-  // };  
-
-  // const onClosePhotoDetailsModal = (photo) => {
-  //   setState((prevState) => ({
-  //     ...prevState,
-  //     selectedPhoto: null,
-  //     isModalOpen: false
-  //   }));  
-  // };  
-
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const checkFavourite = () => {
